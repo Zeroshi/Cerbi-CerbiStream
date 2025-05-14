31fbce94d4950dbdea6be04053582ecf47c88e2b
Thomas Nelson <thomasvnelson@live.com>
2025-05-13 22:02:42 -0400

    added: CerbiStream.Extensions

---
+++ LoggingStandards/Extensions/CerbiLoggerExtensions.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+
+namespace CerbiStream.Extensions
+{
+    public static class CerbiLoggerExtensions
+    {
+        public static void LogInformation(this ILogger logger, object payload)
+        {
+            logger.Log(LogLevel.Information, new EventId(), payload, null, (s, e) => s?.ToString() ?? string.Empty);
+        }
+
+        public static void LogWarning(this ILogger logger, object payload)
+        {
+            logger.Log(LogLevel.Warning, new EventId(), payload, null, (s, e) => s?.ToString() ?? string.Empty);
+        }
+
+        public static CerbiLoggerWrapper Relax(this ILogger logger)
+        {
+            return new CerbiLoggerWrapper(logger, relaxed: true);
+        }
+    }
+
+    public class CerbiLoggerWrapper
+    {
+        private readonly ILogger _logger;
+        private readonly bool _relaxed;
+
+        public CerbiLoggerWrapper(ILogger logger, bool relaxed)
+        {
+            _logger = logger;
+            _relaxed = relaxed;
+        }
+
+        public void LogWarning(object payload)
+        {
+            if (payload is Dictionary<string, object> dict)
+            {
+                dict["GovernanceRelaxed"] = true;
+            }
+
+            _logger.Log(LogLevel.Warning, new EventId(), payload, null, (s, e) => s?.ToString() ?? string.Empty);
+        }
+    }
+}
--- LoggingStandards/GlobalUsings.cs
+++ LoggingStandards/GlobalUsings.cs
@@ -2,4 +2,5 @@
 global using CerbiStream.Classes.FileLogging;
 global using CerbiStream.Telemetry;
 global using CerbiStream.Interfaces;
+global using CerbiStream.Extensions;
 
