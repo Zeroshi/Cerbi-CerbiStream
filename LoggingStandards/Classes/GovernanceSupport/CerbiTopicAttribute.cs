1804270ded0b91949cf742cc7da8c8cf2313e2f9
Thomas Nelson <thomasvnelson@live.com>
2025-05-15 11:58:05 -0400

    added runtime topic

---
+++ LoggingStandards/Classes/GovernanceSupport/CerbiTopicAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Cerbi.Governance
+{
+    /// <summary>
+    /// Used to annotate classes with a governance topic.
+    /// This helps analyzers associate logs with governance profiles.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, Inherited = false, AllowMultiple = false)]
+    public sealed class CerbiTopicAttribute : Attribute
+    {
+        public string TopicName { get; }
+
+        public CerbiTopicAttribute(string topicName)
+        {
+            TopicName = topicName;
+        }
+    }
+}
